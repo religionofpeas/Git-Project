042eca813ece3b27942d53c4ee63e804561522cc
Richard Wiig <richar>
2020-10-17 09:10:59 +1000

    Initial Commit

---
+++ Git Project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Project
+//
+//  Created by Infidel on 17/10/20.
+//  Copyright © 2020 Infidel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

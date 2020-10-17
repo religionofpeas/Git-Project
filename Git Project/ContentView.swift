6cc7a16c787f94e53bdaa8b8b4ecb8e4bbb235a0
Richard Wiig <richar>
2020-10-17 11:15:49 +1000

    Changed button colour to blue.

--- Git Project/ContentView.swift
+++ Git Project/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
           Text("This is the Content View")
           NavigationLink(destination: ModalView()){
             Text("Go Next")
-          }.foregroundColor(.purple)
+          }.foregroundColor(.blue)
         }
         }.navigationBarTitle("Xcode & Git")
     }
